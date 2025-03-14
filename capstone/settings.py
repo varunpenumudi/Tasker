7602d1b93620bc09597eaf6fa74a42c50f1f899f
root <root@varunlaptop>
2025-03-14 13:04:11 +0530

    small changes

--- capstone/settings.py
+++ capstone/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-=eg6ur!b$nq2k4lj7h)&d@lp=gm)vebu!ko6*av)t2f$zpc4*-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['tasker-xiwq.onrender.com']
+ALLOWED_HOSTS = ['tasker-xiwq.onrender.com', '127.0.0.1']
 
 
 # Application definition
@@ -120,6 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
