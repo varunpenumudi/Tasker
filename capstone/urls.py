528eb865c9e335a7ab1e7e9d435e58efc31496a3
root <root@varunlaptop>
2025-03-14 14:14:27 +0530

    changed some static file paths in project

--- capstone/urls.py
+++ capstone/urls.py
@@ -22,4 +22,4 @@ from . import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("tasker.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
